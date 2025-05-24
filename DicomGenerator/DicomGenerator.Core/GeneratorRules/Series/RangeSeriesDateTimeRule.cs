9d9ca893b1d19c99327f9006cedc9f3619c76942
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2025-05-24 19:51:59 +0300

    ref: refactoring Series rules

--- DicomGenerator/DicomGenerator.Core/GeneratorRules/Series/RangeSeriesDateTimeRule.cs
+++ DicomGenerator/DicomGenerator.Core/GeneratorRules/Series/RangeSeriesDateTimeRule.cs
@@ -1,38 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DicomGenerator.Core.GeneratorRules.Study;
-using FellowOakDicom;
 
 namespace DicomGenerator.Core.GeneratorRules.Series
 {
     public sealed class RangeSeriesDateTimeRule : IGeneratorRule<DateTime>
     {
-       
-        //public RangeSeriesDateTimeRule(DateTime dateTime)
-        //{
-        //    SeriesTime = dateTime;
 
-        //}
-        
-        public DateTime SeriesTime { get; }
+        public RangeSeriesDateTimeRule(DateTime startDateTime, DateTime endDateTime)
+        {
+            MinSeriesTime = startDateTime;
+            MaxSeriesTime = endDateTime;            
+        }
+
+        public DateTime MaxSeriesTime { get; }
+        public DateTime MinSeriesTime { get; }
 
         public DateTime Generate()
         {
             var random = new Random();
 
-            DateTime nowDate = DateTime.Now;
+            int rndHours = random.Next(MinSeriesTime.Hour, MaxSeriesTime.Hour);
+            int rndMinute = random.Next(MinSeriesTime.Minute, MaxSeriesTime.Minute);
 
-            int rndHours = random.Next(0, 24);
-            int rndMinute = random.Next(0, 60);
-            int rndSecond = random.Next(0, 60);
-
-            var seriesTime = SeriesTime.AddHours(random.Next(rndHours)).AddMinutes(random.Next(rndMinute)).AddSeconds(random.Next(rndSecond));
+            var seriesTime = MinSeriesTime.AddHours(rndHours).AddMinutes(rndMinute);
 
             return seriesTime;
-
         }
     }
 }
