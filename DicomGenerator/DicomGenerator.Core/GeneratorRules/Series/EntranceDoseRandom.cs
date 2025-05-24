bd674bf35a5dbcf8c126c6c88b9b8be0b1fe6b30
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2025-05-24 19:19:17 +0300

    ref: create rules for Series

---
+++ DicomGenerator/DicomGenerator.Core/GeneratorRules/Series/EntranceDoseRandom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicomGenerator.Core.GeneratorRules.Series
+{
+    internal class EntranceDoseRandom
+    {
+        private static IDictionary<int, decimal> _doseValues = new Dictionary<int, decimal>
+        {
+            {0, 0.120m},
+            {1, 0.250m},
+            {2, 0.080m},
+            {3, 0.300m},
+            {4, 0.100m},
+            {5, 0.450m},
+            {6, 0.220m},
+            {7, 0.500m},
+            {8, 0.050m},
+            {9, 0.380m}
+        };
+
+        public static decimal GenerateDose()
+        {
+            var random = new Random();
+            var dose = _doseValues[random.Next(0, _doseValues.Count)];
+            return dose * 1000;
+        }
+    }
+}
