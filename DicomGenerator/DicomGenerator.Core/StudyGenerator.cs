79b36b1129cc560e82a363c37a81e1c6bf555300
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2025-05-24 19:56:54 +0300

    ref:refactoring Study generator method

--- DicomGenerator/DicomGenerator.Core/StudyGenerator.cs
+++ DicomGenerator/DicomGenerator.Core/StudyGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using DicomGenerator.Core.GeneratorRules.Study;
 using FellowOakDicom;
 
 namespace DicomGenerator.Core
@@ -7,7 +8,7 @@ namespace DicomGenerator.Core
     internal class StudyGenerator
     {
         public IEnumerable<DicomDataset> Generate(
-            DicomDataset patientIod, 
+            DicomDataset patientIod,
             StudyGeneratorParameters studyGeneratorParameters)
         {
             var dataSets = new List<DicomDataset>();
@@ -36,10 +37,34 @@ namespace DicomGenerator.Core
             dataset.AddOrUpdate(DicomTag.StudyTime, new DateTime(studyDateTime.TimeOfDay.Ticks));
             dataset.AddOrUpdate(DicomTag.AccessionNumber, studyGeneratorParameters.AccessionNumber.Generate());
             dataset.AddOrUpdate(DicomTag.StudyInstanceUID, UidGenerator.GenerateStudyUid());
+            dataset.AddOrUpdate(DicomTag.OperatorsName, OperatorsNameGenerator.GenerateOperatorsName());
+            dataset.AddOrUpdate(DicomTag.StudyID, StudyIdGenerator.GenerateStudyId());
+            dataset.AddOrUpdate(DicomTag.Manufacturer, "AutoGenFILE");
+
+
+            BodyPartRandom.GenerateRegion();
+            BodyPartRandom.GenerateBodyPositions();
+
+            var regionItem = new DicomDataset
+            {
+                { DicomTag.CodeValue, BodyPartRandom.RegionCodeValue },
+                { DicomTag.CodingSchemeDesignator, BodyPartRandom.RegionCodingSchemeDesignator },
+                { DicomTag.CodeMeaning, BodyPartRandom.RegionCodeMeaning }
+            };
+            dataset.AddOrUpdate(DicomTag.AnatomicRegionSequence, regionItem);
+
+            var viewItem = new DicomDataset
+            {
+                { DicomTag.CodeValue, BodyPartRandom.ViewCodeValue },
+                { DicomTag.CodingSchemeDesignator, BodyPartRandom.ViewCodingSchemeDesignator },
+                { DicomTag.CodeMeaning, BodyPartRandom.ViewCodeMeaning },
+                { DicomTag.ContextUID, BodyPartRandom.ViewContextUID }
+            };
+            dataset.AddOrUpdate(DicomTag.ViewCodeSequence, viewItem);
+            dataset.AddOrUpdate(DicomTag.BodyPartExamined, BodyPartRandom.BodyPartExamined);
+            dataset.AddOrUpdate(DicomTag.ImageLaterality, BodyPartRandom.GenerateLaterality());
 
             return dataset;
-
-            
         }
     }
 }
