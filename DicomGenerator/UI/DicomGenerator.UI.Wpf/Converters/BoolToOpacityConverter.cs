a93d2a0ca93c6698c40405bbf247d69046e8ae63
ADANI\zabrodin <rediskal50@gmail.com>
2026-07-16 17:09:23 +0300

    ref: refactoring and add converter

---
+++ DicomGenerator/UI/DicomGenerator.UI.Wpf/Converters/BoolToOpacityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DicomGenerator.UI.Wpf.Converters
+{
+    public class BoolToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString() == parameter?.ToString() ? 1.0 : 0.4;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DicomGenerator/UI/DicomGenerator.UI.Wpf/Converters/EchoStatusToColorConverter.cs
+++ DicomGenerator/UI/DicomGenerator.UI.Wpf/Converters/EchoStatusToColorConverter.cs
@@ -9,15 +9,15 @@ namespace DicomGenerator.UI.Wpf.Converters
     public class EchoStatusToColorConverter : IValueConverter
     {
         private static readonly Dictionary<string, Brush> _statusColors = new()
-    {
-        { "successful", Brushes.YellowGreen },
-        { "success", Brushes.YellowGreen },
-        { "error", Brushes.Red },
-        { "failed", Brushes.Red },
-        { "fail", Brushes.Red },
-        { "connecting", Brushes.Orange },
-        { "please enter", Brushes.Orange },
-    };
+        {
+            { "successful", Brushes.YellowGreen },
+            { "success", Brushes.YellowGreen },
+            { "error", Brushes.Red },
+            { "failed", Brushes.Red },
+            { "fail", Brushes.Red },
+            { "connecting", Brushes.Orange },
+            { "please enter", Brushes.Orange },
+        };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
