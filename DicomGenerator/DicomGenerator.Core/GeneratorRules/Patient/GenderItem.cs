e425a207b5fb764cebfb33d92d8994529f1468c5
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2026-07-21 23:50:18 +0300

    ref: add display gender

---
+++ DicomGenerator/DicomGenerator.Core/GeneratorRules/Patient/GenderItem.cs
@@ -0,0 +1,8 @@
+﻿namespace DicomGenerator.Core.GeneratorRules.Patient
+{
+    public class GenderItem
+    {
+        public string DisplayName { get; set; }
+        public string Code { get; set; }
+    }
+}
