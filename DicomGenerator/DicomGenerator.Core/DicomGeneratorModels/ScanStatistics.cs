f85bc6d6cd55d2d0b11d9fc2e17af9115715887d
ADANI\zabrodin <rediskal50@gmail.com>
2026-08-07 13:28:27 +0300

    ref: create scan Statistics

---
+++ DicomGenerator/DicomGenerator.Core/DicomGeneratorModels/ScanStatistics.cs
@@ -0,0 +1,21 @@
+﻿namespace DicomGenerator.Core.DicomGeneratorModels
+{
+    public class ScanStatistics
+    {
+        public int Patients { get; set; }
+        public int Studies { get; set; }
+        public int Series { get; set; }
+        public int Images { get; set; }
+
+        public static ScanStatistics operator +(ScanStatistics left, ScanStatistics right)
+        {
+            return new ScanStatistics
+            {
+                Patients = left.Patients + right.Patients,
+                Studies = left.Studies + right.Studies,
+                Series = left.Series + right.Series,
+                Images = left.Images + right.Images
+            };
+        }
+    }
+}
