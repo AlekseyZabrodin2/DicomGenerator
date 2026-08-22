b18dbcd5178a6cd93243f1ef229cd8cac63d18b1
ADANI\zabrodin <rediskal50@gmail.com>
2026-08-22 16:06:21 +0300

    feat: create time estimate service

---
+++ DicomGenerator/UI/DicomGenerator.UI.Wpf/DicomFileParser/ScanTimeEstimator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DicomGenerator.UI.Wpf.DicomFileParser
+{
+    public sealed class ScanTimeEstimator
+    {
+        private double _totalSeconds;
+        private int _completed;
+
+        public void Add(TimeSpan elapsed)
+        {
+            _totalSeconds += elapsed.TotalSeconds;
+            _completed++;
+        }
+
+        public TimeSpan GetRemainingTime(int remaining)
+        {
+            if (_completed == 0)
+                return TimeSpan.Zero;
+
+            var averageSeconds = _totalSeconds / _completed;
+
+            return TimeSpan.FromSeconds(averageSeconds * remaining);
+        }
+
+        public string FormatTimeSpan(TimeSpan time)
+        {
+            if (time.TotalHours >= 1)
+            {
+                return $"{time.Hours} ч {time.Minutes} мин {time.Seconds} сек";
+            }
+            if (time.TotalMinutes >= 1)
+            {
+                return $"{time.Minutes} мин {time.Seconds} сек";
+            }
+            return $"{time.TotalSeconds:F1} сек";
+        }
+    }
+}
