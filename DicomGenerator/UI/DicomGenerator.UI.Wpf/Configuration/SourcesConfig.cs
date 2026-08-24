8c386276f022f2a142af3100387b4441ffd5ff2b
ADANI\zabrodin <rediskal50@gmail.com>
2026-08-24 11:22:40 +0300

    ref: add DataBasePath in config

--- DicomGenerator/UI/DicomGenerator.UI.Wpf/Configuration/SourcesConfig.cs
+++ DicomGenerator/UI/DicomGenerator.UI.Wpf/Configuration/SourcesConfig.cs
@@ -4,6 +4,7 @@ namespace DicomGenerator.UI.Wpf.Configuration
 {
     public class SourcesConfig
     {
+        public string DataBasePath { get; set; } = string.Empty;
         public List<FolderConfig> Folders { get; set; } = [];
         public List<PacsConfig> Pacs { get; set; } = [];
     }
